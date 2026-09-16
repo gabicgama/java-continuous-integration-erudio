a50467fdb7e25b6db58f4a83c2d964bb2dc73e51
Gabriela Gama <gabycgama@hotmail.com>
2026-09-16 16:19:19 -0300

    criando model, controller, service e repository da classe Person

---
+++ src/main/java/br/com/erudio/controller/PersonController.java
@@ -0,0 +1,54 @@
+package br.com.erudio.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.service.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	private final PersonService service;
+
+	public PersonController(PersonService service) {
+		this.service = service;
+	}
+
+	@GetMapping()
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public Person findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+	@PostMapping()
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+
+	@PutMapping()
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
